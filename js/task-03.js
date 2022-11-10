6c403c9122d513866bbdbafe10657b3da01cf7be
Ihor Buhaichuk <ihorbugaichuk@gmail.com>
2022-11-10 15:37:12 +0200

    123

--- js/task-03.js
+++ js/task-03.js
@@ -1,31 +1,41 @@
 const images = [
   {
-    src: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'White and Black Long Fur Cat',
   },
   {
-    src: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
   },
   {
-    src: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'Group of Horses Running',
   },
 ];
 
 const gallery = document.querySelector('.gallery');
 
-const arr = [];
+const image = images.map(elem => {
+  const url = elem.url
+  const alt = elem.alt
 
-for (let i = 0; i < images.length; i += 1) {
-  const newItem = document.createElement('li');
-  const newImg = document.createElement('img');
-  newImg.classList.add('size');
-  newImg.setAttribute('src', images[i].src);
-  newImg.setAttribute('alt', images[i].alt);
-  newItem.append(newImg);
-  console.log(newItem);
-  arr.push(newItem);
-}
+  return `<li class "gallery-item"> <img src="${url}" alt="${alt}" width = "350px"</li>`
+})
 
-gallery.append(...arr);
+gallery.insertAdjacentHTML("afterbegin", image)
+
+
+// const arr = [];
+
+// for (let i = 0; i < images.length; i += 1) {
+//   const newItem = document.createElement('li');
+//   const newImg = document.createElement('img');
+//   newImg.classList.add('size');
+//   newImg.setAttribute('src', images[i].src-7);
+//   newImg.setAttribute('alt', images[i].alt);
+//   newItem.append(newImg);
+//   console.log(newItem);
+//   arr.push(newItem);
+// }
+
+// gallery.append(...arr);
